58f4315492568a2598e47e5b483d44b3697593b0
Sanskar Bajirao Alave <141243786+Sanskar077@users.noreply.github.com>
2026-05-24 21:53:06 +0530

    Day 4

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
+const fs = require("fs");
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const { Deepgram } = require("@deepgram/sdk");
 
+const deepgram = new Deepgram(
+  process.env.DEEPGRAM_API_KEY
+);
 const app = express();
 
 app.use(cors());
@@ -24,11 +29,38 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-app.post("/upload", upload.single("audio"), (req, res) => {
-  res.json({
-    message: "File uploaded successfully",
-    file: req.file,
-  });
+app.post("/upload", upload.single("audio"), async (req, res) => {
+  try {
+    const audio = fs.readFileSync(req.file.path);
+
+    const response = await deepgram.transcription.preRecorded(
+      {
+        buffer: audio,
+        mimetype: req.file.mimetype,
+      },
+      {
+        punctuate: true,
+        model: "nova",
+        language: "en-US",
+      }
+    );
+
+    const text =
+      response.results.channels[0]
+      .alternatives[0].transcript;
+
+    res.json({
+      message: "Transcription successful",
+      text,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Transcription failed",
+    });
+  }
 });
 
 mongoose
